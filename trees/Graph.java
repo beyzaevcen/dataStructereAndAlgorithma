d671db3d8ad99c4cf8cc4fb49f307baa2c5b8ac4
Beyza Evcen <beyzaevcen37@icloud.com>
2022-12-15 15:34:03 +0300

    Initial commit

--- trees/Graph.java
+++ trees/Graph.java
@@ -15,15 +15,57 @@ public class Graph <T extends Comparable>{
         return null;
     }
     public int inDegree(String id){
-return 0;
+       return 0;
+
     }
     public int outDegree(String id){
-        return 0;
+        Vertex<T> current=findVertex(id);
+        int counter=0;
+        if(current==null){
+            System.out.println("Not exist vertex");
+        }else{
+            Edge iterator=current.EdgeLink;
+            if(iterator==null){
+                System.out.println("0 outdegree");
+            }else{
+                while(iterator!=null){
+                    counter++;
+                    iterator=iterator.nextEdge;
+                }
+            }
+        }
+        return counter;
     }
     public void addVertex(String id){
+        if(findVertex(id)==null) {
+            if (head == null) {
+                head = new Vertex<T>(id);
+            }
+            Vertex<T> ite = head;
+            while (ite.nextVertex != null) {
+                ite = ite.nextVertex;
+            }
+            ite.nextVertex = new Vertex<T>(id);
+        }
+        System.out.println("exist");
 
     }
     public void addEdge(String idStart,String idEnd,int weight){
+        Vertex<T> start=findVertex(idStart);
+        Vertex<T> end=findVertex(idEnd);
+        if(start!=null&&end!=null){
+            Edge iterator=start.EdgeLink;
+            if(iterator==null){
+                start.EdgeLink=new Edge(idEnd,weight);
+            }else{
+                while(iterator.nextEdge!=null){
+                    iterator=iterator.nextEdge;
+                }
+                iterator.nextEdge=new Edge(idEnd,weight);
+            }
+        }else{
+            System.out.println("This edge can not exist");
+        }
 
     }
     //list of neighbors of given vertex
